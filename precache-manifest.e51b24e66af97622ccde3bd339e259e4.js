d3a9317369230efcecd67613c484b3faf97a9976
Szymek <mail@m.pl>
2019-06-05 04:30:15 +0200

    Updates

---
+++ precache-manifest.e51b24e66af97622ccde3bd339e259e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cc207b6f298a38d82441e878485da5",
+    "url": "/crypto_users/index.html"
+  },
+  {
+    "revision": "2be002112624f721eadd",
+    "url": "/crypto_users/static/css/main.114c18c5.chunk.css"
+  },
+  {
+    "revision": "471263e36fe8c73db02e",
+    "url": "/crypto_users/static/js/2.881f9530.chunk.js"
+  },
+  {
+    "revision": "2be002112624f721eadd",
+    "url": "/crypto_users/static/js/main.64a91bd0.chunk.js"
+  },
+  {
+    "revision": "e273332c6abfa00ee372",
+    "url": "/crypto_users/static/js/runtime~main.a968dabe.js"
+  }
+]);
